e5712a3d8ae6994d04620ef580df2380d91c88d2
miguel <miguelmu@kth.se>
2018-11-14 01:37:10 +0100

    Added option to put all character names in players.csv

--- browserfiles/images/fetch_images/fetch_char_images.sh
+++ browserfiles/images/fetch_images/fetch_char_images.sh
@@ -3,4 +3,5 @@ mkdir $folder
 while read -r filename url;
 do
   wget https://www.ssbwiki.com${url} -O ${folder}/${filename}
+  #echo ",,${filename}" | cut -d'.' -f 1 >> ../../../players.csv    #use this for autocompletion of characters in SC
 done <char_img_to_url.txt
