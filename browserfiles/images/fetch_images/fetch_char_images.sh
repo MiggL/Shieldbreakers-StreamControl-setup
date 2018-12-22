d384c3d5e78a1880dbf06c72091a751a02d4e6b0
miguel <miguelmu@kth.se>
2018-12-23 00:46:26 +0100

    Added alt costumes

--- browserfiles/images/fetch_images/fetch_char_images.sh
+++ browserfiles/images/fetch_images/fetch_char_images.sh
@@ -2,6 +2,8 @@ folder="../smash_ultimate"
 mkdir $folder
 while read -r filename url;
 do
-  wget https://www.ssbwiki.com${url} -O ${folder}/${filename}
-  #echo ",,${filename}" | cut -d'.' -f 1 >> ../../../players.csv    #use this for autocompletion of characters in SC
+  wget https://www.ssbwiki.com${url} -O ${filename}
+  convert ${filename} -crop 100x100 -scene 1 ${folder}/${filename:0:-4}%d.png
+  rm ${filename}
+  echo ",,${filename:0:-4}" >> ../../../players.csv    #use this for autocompletion of characters in SC
 done <char_img_to_url.txt
