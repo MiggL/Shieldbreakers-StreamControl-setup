3386128ea057f6fd0479508d9484a496b6581b64
miguel <miguelmu@kth.se>
2018-12-23 01:28:18 +0100

    Copyright & licensing

--- browserfiles/js/common.js
+++ browserfiles/js/common.js
@@ -1,3 +1,22 @@
+/**
+ * Copyright 2018 Miguel Müller
+ * 
+ * This file is part of Shieldbreakers-StreamControl-setup.
+ *
+ * Shieldbreakers-StreamControl-setup is free software: you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Shieldbreakers-StreamControl-setup is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * Shieldbreakers-StreamControl-setup.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function getValueFromTag (xmlDoc,tag) {
   if (xmlDoc.getElementsByTagName(tag).length != 0 ) {
     if (xmlDoc.getElementsByTagName(tag)[0].childNodes.length == 0) {
